2bd7d746c59d50e5db5d7f66a5ad1dcfd2ca06bb
Shino Lim <shino@comp.nus.edu.sg>
2023-03-30 20:18:54 +0800

    Added FillerPacker and TransitCellar

--- contracts/FillerPacker.sol
+++ contracts/FillerPacker.sol
@@ -4,4 +4,74 @@ import "./Product.sol";
 
 contract FillerPacker {
     
+    //variables
+    Product productContract;
+    mapping(uint256 => string) private packagingDetails; // Packaging details of each product
+    mapping(uint256 => string) private labelDetails; // Label details of each product
+
+    //events
+    event wineReceived(uint productId);
+    event winePackaged(uint productId); 
+    event wineLabelled(uint productId); 
+    event wineDispatched(uint productId);
+
+    //modifiers
+    modifier ownerOnly(uint256 productId) {
+        require(productContract.getCurrentOwner(productId) == msg.sender);
+        _;
+    }
+
+    // modifier validProductId(uint256 productId) {
+    //     require(productId < productContract.noProduct);
+    //     _;
+    // }
+
+    //methods
+    function getOwner() public view returns(address) {
+       return owner;
+    }
+
+    function getPackagingDetails(uint256 productId) public view returns(string memory) {
+       return packagingDetails[productId];
+    }
+
+    function getLabelDetails(uint256 productId) public view returns(string memory) {
+       return labelDetails[productId];
+    }
+
+    function getDispatchDetails(uint256 productId) public view  returns(string memory) {
+       return dispatchDates[productId];
+    }
+
+    function receiveWineFromTransitCellar(uint256 productId, string memory currentLocation, string memory arrivalDate) public ownerOnly(productId) {
+        (string memory location, string memory dispatchDate, string memory prevArrivalDate) = productContract.getCurrentLocation(productId);
+        productContract.addPreviousLocation(productId, location, dispatchDate, prevArrivalDate);
+        productContract.setCurrentLocation(productId, currentLocation, "", arrivalDate);
+        
+        productContract.setReceived(productId, true);
+        
+        emit wineReceived(productId);
+    }
+
+    function packageWine(uint256 productId, string memory packagingDetail) public {
+        packagingDetails[productId] = packagingDetail;
+        emit winePackaged(productId);
+    }
+
+    function labelWine(uint256 productId, string memory labelDetail) public {
+        labelDetails[productId] = labelDetail;
+        emit wineLabelled(productId);
+    }
+
+    function dispatchWineToGoodsDistributor(uint256 productId, string memory labelDetail, string memory dispatchDate, address newOwner, address newContractAddress) public ownerOnly(productId) {
+        (string memory location, string memory prevDispatchDate, string memory prevArrivalDate) = productContract.getCurrentLocation(productId);
+        productContract.addPreviousLocation(productId, location, dispatchDate, prevArrivalDate);
+        productContract.setCurrentLocation(productId, "", "", "");
+        
+        productContract.transferProduct(productId, newOwner, newContractAddress);
+
+        productContract.setReceived(productId, false);
+        
+        emit wineDispatched(productId);
+    }
 }
--- contracts/TransitCellar.sol
+++ contracts/TransitCellar.sol
@@ -4,4 +4,62 @@ import "./Product.sol";
 
 contract TransitCellar {
     
+    //variables
+    Product productContract;
+    address owner = msg.sender;
+    mapping(uint256 => string) private analysisDetails; // Analysis details of each product
+
+    //events
+    event wineReceived(uint productId);
+    event wineAnalysed(uint productId); 
+    event wineDispatched(uint productId);
+
+    //modifiers
+    modifier ownerOnly(uint256 productId) {
+        require(productContract.getCurrentOwner(productId) == msg.sender);
+        _;
+    }
+
+    // modifier validProductId(uint256 productId) {
+    //     require(productId < productContract.noProduct);
+    //     _;
+    // }
+
+    //methods
+    function getAnalysisDetails(uint256 productId) public view returns(string memory) {
+       return analysisDetails[productId];
+    }
+
+    function getDispatchDetails(uint256 productId) public view returns(string memory) {
+       return dispatchDates[productId];
+    }
+
+    function receiveWineFromBulkDistributor(uint256 productId, string memory currentLocation, string memory arrivalDate) public payable ownerOnly(productId) {
+        (string memory location, string memory dispatchDate, string memory prevArrivalDate) = productContract.getCurrentLocation(productId);
+        productContract.addPreviousLocation(productId, location, dispatchDate, prevArrivalDate);
+        productContract.setCurrentLocation(productId, currentLocation, "", arrivalDate);
+        
+        productContract.setReceived(productId, true);
+        
+        emit wineReceived(productId);
+    }
+
+    function analyseWine(uint256 productId, string memory analysisDetail) public {
+        analysisDetails[productId] = analysisDetail;
+        emit wineAnalysed(productId);
+    }
+
+    function dispatchWineToFillerPacker(uint256 productId, string memory dispatchDate, address newOwner, address newContractAddress) public payable ownerOnly(productId) {
+        analysisDetails[productId] = analysisDetail;
+
+        (string memory location, string memory prevDispatchDate, string memory prevArrivalDate) = productContract.getCurrentLocation(productId);
+        productContract.addPreviousLocation(productId, location, dispatchDate, prevArrivalDate);
+        productContract.setCurrentLocation(productId, "", "", "");
+
+        productContract.transferProduct(productId, newOwner, newContractAddress);
+        productContract.setReceived(productId, false);
+        
+        emit wineDispatched(productId);
+    }
+
 }
